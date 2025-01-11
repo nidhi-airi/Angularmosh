96c6286ddebe6eb0c0b9dbbbe699e07f61fbc2e6
Nidhi <vfx@gmail.com>
2025-01-11 18:10:00 +0530

    two way data binding

--- new_angular/src/app/components/roles/roles.component.ts
+++ new_angular/src/app/components/roles/roles.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-
+  firstName:string="Angular Tutorial";
+  angularVersion="Version18";
+  version:number=18;
+  isActive:boolean=false;
+  currentDate: Date=new Date()
+  inputType:string="checkbox"
 }
